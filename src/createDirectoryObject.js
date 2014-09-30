ec1b3ccff394fd8d9076115140479ce742b1c44e
Travis Wimer <accounts@traviswimer.com>
2014-09-30 17:44:41 -0400

    Add basic setup

---
+++ src/createDirectoryObject.js
@@ -0,0 +1,7 @@
+var fs = require("fs");
+
+module.exports = function( path, options, callback ){
+
+
+
+};
--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 var fs = require("fs");
+var createDirectoryObject = require("./createDirectoryObject");
 
-module.exports = function( path, callback, filter ){
-	
+module.exports = function( path, options, callback ){
+
+	if( typeof path !== "string" ){
+		callback( new Error("Path parameter must be a string") );
+		return;
+	}
+
+	if( typeof options === "function" ){
+		callback = options;
+	}
+
+	if( typeof callback !== "function" ){
+		callback( new Error("A callback function must be provided") );
+		return;
+	}
+
+	createDirectoryObject( path, options, function( err, obj ){
+		callback( err, obj, JSON.stringify(obj) );
+	});
+
 };
