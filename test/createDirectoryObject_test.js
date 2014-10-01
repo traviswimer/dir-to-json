196138f3dd9cddf2dbc1fd1a46ed5b908c51f8b0
Travis Wimer <accounts@traviswimer.com>
2014-10-01 19:57:15 -0400

    Add heirarchy tests

--- test/createDirectoryObject_test.js
+++ test/createDirectoryObject_test.js
@@ -65,7 +65,88 @@ describe("createDirectoryObject", function(){
 			createDirectoryObject.__set__( 'readdir', readdirStub );
 
 			createDirectoryObject( path, "" ).then( function( data ){
-				expect( data.children ).not.to.be.undefined;
+				expect( data.children.length ).to.equal( files.length );
+				done();
+			});
+		});
+
+
+		it("should produce a tree across multiple levels", function( done ){
+
+			var path = "fakeDir";
+			var files1 = [
+				"fakeDir",
+				"fakeDir",
+				"fakeFile",
+				"fakeFile"
+			];
+			var files2 = [
+				"fakeDir",
+				"fakeFile"
+			];
+			var files3 = [
+				"fakeFile",
+				"fakeFile"
+			];
+
+			var statStub = sinon.stub();
+			statStub
+				.onCall(0)
+				.returns( Q.resolve({
+					isFile: function(){
+						return false;
+					}
+				}))
+				.onCall(1)
+				.returns( Q.resolve({
+					isFile: function(){
+						return false;
+					}
+				}))
+				.onCall(2)
+				.returns( Q.resolve({
+					isFile: function(){
+						return false;
+					}
+				}))
+				.onCall(5)
+				.returns( Q.resolve({
+					isFile: function(){
+						return false;
+					}
+				}));
+
+			statStub.returns( Q.resolve({
+				isFile: function(){
+					return true;
+				}
+			}));
+
+			var readdirStub = sinon.stub();
+
+			readdirStub
+				.onCall(0)
+				.returns( Q.resolve( files1 ) )
+				.onCall(1)
+				.returns( Q.resolve( files2 ) )
+				.onCall(2)
+				.returns( Q.resolve( files3 ) );
+
+			createDirectoryObject.__set__( 'stat', statStub );
+			createDirectoryObject.__set__( 'readdir', readdirStub );
+
+			createDirectoryObject( path, "" ).then( function( tree ){
+				expect( tree.children.length ).to.equal( files1.length );
+
+				var subDir1 = tree.children[0];
+				var subDir2 = tree.children[1];
+
+				expect( subDir1.children.length ).to.equal( files2.length );
+				expect( subDir2.children.length ).to.equal( files3.length );
+
+				var subSubDir = subDir1.children[0];
+				expect( subSubDir.children.length ).to.equal( 0 );
+				
 				done();
 			});
 		});
