3da7d236571f7a31aad89af6afd05e3db68180e1
Travis Wimer <accounts@traviswimer.com>
2014-10-01 14:16:55 -0400

    Add tests

--- test/createDirectoryObject_test.js
+++ test/createDirectoryObject_test.js
@@ -11,22 +11,64 @@ describe("createDirectoryObject", function(){
 	beforeEach(function(){
 		// Make sure fs methods are always empty by default
 		createDirectoryObject.__set__( 'stat', function( currentDir ){} );
+		createDirectoryObject.__set__( 'readdir', function( currentDir ){} );
 	});
 
-	it("should not have children if file", function( done ){
+	describe("file", function(){
 
-		var statStub = sinon.stub().returns( Q.resolve({
-			isFile: function(){
-				return true;
-			}
-		}));
+		it("should resolve with no children", function( done ){
 
-		createDirectoryObject.__set__( 'stat', statStub );
+			var statStub = sinon.stub().returns( Q.resolve({
+				isFile: function(){
+					return true;
+				}
+			}));
 
-		var path = "./fakeFile.txt";
-		createDirectoryObject( path, "" ).then( function( data ){
-			expect( data.childrend ).to.be.undefined;
-			done();
+			createDirectoryObject.__set__( 'stat', statStub );
+
+			var path = "./fakeFile.txt";
+			createDirectoryObject( path, "" ).then( function( data ){
+				expect( data.children ).to.be.undefined;
+				done();
+			});
 		});
+
+	});
+
+	describe("directory", function(){
+
+		it("should resolve with children", function( done ){
+
+			var path = "./fakeDir";
+			var files = [
+				"fakeFile1",
+				"fakeFile2",
+				"fakeFile3"
+			];
+
+			var statStub = sinon.stub();
+			statStub.onCall(0).returns( Q.resolve({
+				isFile: function(){
+					return false;
+				}
+			}));
+
+			statStub.returns( Q.resolve({
+				isFile: function(){
+					return true;
+				}
+			}));
+
+			var readdirStub = sinon.stub().returns( Q.resolve( files ));
+
+			createDirectoryObject.__set__( 'stat', statStub );
+			createDirectoryObject.__set__( 'readdir', readdirStub );
+
+			createDirectoryObject( path, "" ).then( function( data ){
+				expect( data.children ).to.be.defined;
+				done();
+			});
+		});
+
 	});
 });
